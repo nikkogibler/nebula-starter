5db95dd724773c8bae6f39fc78775405994e4143
nikkogibler <nikkogibler@gmail.com>
2025-04-24 20:20:23 -0600

    Update SignIn.js
    Added Sign In code

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,1 +1,27 @@
 
+import { supabase } from '../lib/supabaseClient';
+
+export default function SignIn() {
+  const handleGoogleLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+    });
+  };
+
+  return (
+    <button
+      onClick={handleGoogleLogin}
+      style={{
+        padding: '12px 20px',
+        fontSize: '16px',
+        background: 'white',
+        color: 'black',
+        borderRadius: '8px',
+        marginTop: '40px',
+        cursor: 'pointer'
+      }}
+    >
+      Sign in with Google
+    </button>
+  );
+}
